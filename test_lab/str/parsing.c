7bb266c50bb115ca616e8358a4d9efc8f8c0cd75
Taejun Choi <tjchoi@zaram.com>
2018-07-25 14:30:45 +0900

    refactorying parsing

--- test_lab/str/parsing.c
+++ test_lab/str/parsing.c
@@ -111,16 +111,108 @@ void get_client_list()
 	return;
 }
 
+void get_client_list2()
+{
+	int i = 0, line_num;
+	FILE *fp;
+	char cmd[20];
+	char buf[1024];
+	char device[10];
+	uint8_t ip[4];
+	uint8_t mac[6];
+
+	const char *start_line, *target_words;
+	int ret;
+	snprintf(cmd, 20, "cat /proc/net/arp");
+
+	fp = popen(cmd, "r");
+	if (fp == NULL) {
+		printf("Error: popen fail ! \n");
+		return;
+	}
+
+	while (fgets(buf, sizeof(buf), fp))
+	{
+		target_words = get_words(buf, 5);
+		sscanf(target_words, "%s ", device);
+		if (!strncmp(device, "eth0", 5))
+		{
+			//printf("%d:%s", i, buf);
+			target_words = get_words(buf, 0);
+			sscanf(target_words, "%d.%d.%d.%d ", (int *)&ip[0], (int *)&ip[1],
+												(int *)&ip[2], (int *)&ip[3]);
+			printf("%d.%d.%d.%d\t\t", ip[0], ip[1], ip[2], ip[3]);
+
+			target_words = get_words(buf, 3);
+			sscanf(target_words, "%02X:%02X:%02X:%02X:%02X:%02X ",
+					(unsigned int *)&mac[0], (unsigned int *)&mac[1],
+					(unsigned int *)&mac[2], (unsigned int *)&mac[3],
+					(unsigned int *)&mac[4], (unsigned int *)&mac[5]);
+			printf("%02X:%02X:%02X:%02X:%02X:%02X\n",
+					mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+		}
+		i++;
+	}
+/*
+	start_line = strpbrk(buf, "\n") + 1;
+	printf("%s\n\n", start_line);
+
+	line_num = get_newline_num(buf, sizeof(buf));
+	printf("line:%d\n", line_num);
+
+	for (i = 0 ; i < line_num ; i++)
+	{
+		target_words = get_words(start_line, 5);
+		sscanf(target_words, "%s ", device);
+		if (!strncmp(device, "eth0", 5))
+		{
+			target_words = get_words(start_line, 0);
+			sscanf(target_words, "%d.%d.%d.%d ", (int *)&ip[0], (int *)&ip[1], 
+												(int *)&ip[2], (int *)&ip[3]);
+			printf("%d.%d.%d.%d\n", ip[0], ip[1], ip[2], ip[3]);
+
+			target_words = get_words(start_line, 3);
+			sscanf(target_words, "%02X:%02X:%02X:%02X:%02X:%02X ",
+					(unsigned int *)&mac[0], (unsigned int *)&mac[1], 
+					(unsigned int *)&mac[2], (unsigned int *)&mac[3], 
+					(unsigned int *)&mac[4], (unsigned int *)&mac[5]);
+			printf("%02X:%02X:%02X:%02X:%X:%X\n", 
+					mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+
+		}
+
+
+		start_line = strpbrk(start_line, "\n") + 1;
+		//printf("%s\n", start_line);
+	
+	}
+*/
+	return;
+}
+
 void test1()
 {
+	printf("##### test1! #####\n");
 	get_client_list();
+
+	return;
+}
+
+void test2()
+{
+	printf("##### test2! #####\n");
+	
+	get_client_list2();
 
 	return;
 }
 
 int main()
 {
-	test1();
+//	test1();
+	
+	/* refactoring version */
+	test2();
 
 	return 0;
 }
